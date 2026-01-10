9817852bbef45db809e9874c06708f3363e66d15
imran23105 <professional.imran023@gmail.com>
2026-01-10 18:46:31 +0530

    hbhh

--- Server/App/Routes/visionRoutes.js
+++ Server/App/Routes/visionRoutes.js
@@ -20,12 +20,14 @@
 
 const express = require("express");
 const router = express.Router();
-const upload = require("../middlewares/upload");
-const { analyzeImage } = require("../Controllers/VisionController");
+
+const upload = require("../Middleware/upload");
+const { analyzeImage } = require("../Controller/visionController");
 
 router.post("/image", upload.single("image"), analyzeImage);
 
 module.exports = router;
+
 
 
 
