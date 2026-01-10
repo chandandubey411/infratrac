ee29457410e3188ca08c74a4efe2de5060b93a2f
imran23105 <professional.imran023@gmail.com>
2026-01-10 10:42:58 +0530

    Issue

--- Server/App/Middleware/upload.js
+++ Server/App/Middleware/upload.js
@@ -1,10 +1,19 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "civic-issues",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+  },
 });
 
 module.exports = multer({ storage });
